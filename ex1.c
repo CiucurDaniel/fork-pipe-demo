5c96c71f647bd4a7062436f14779b99d3deef1e0
Ciucur Daniel <ciucur.daniel14@gmail.com>
2020-02-18 20:35:22 +0200

    Add files via upload

---
+++ ex1.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h> 
+#include <ctype.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+
+
+
+    return 0;
+}
